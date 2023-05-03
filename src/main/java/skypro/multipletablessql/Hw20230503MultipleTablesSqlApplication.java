59ea425606383b06b540db0a9d6a8c152bcb8701
Qwaieare <gb-grand@mail.ru>
2023-05-03 23:32:12 +0300

    initial commit for hw20230503_multipleTablesSQL

---
+++ src/main/java/skypro/multipletablessql/Hw20230503MultipleTablesSqlApplication.java
@@ -0,0 +1,13 @@
+package skypro.multipletablessql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw20230503MultipleTablesSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw20230503MultipleTablesSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/multipletablessql/Hw20230503MultipleTablesSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.multipletablessql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20230503MultipleTablesSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
